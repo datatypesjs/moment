8ed7cfc02aa2314a7575b153ef0f3278e6a421e1
Adrian Sieber <mail@adriansieber.com>
2016-03-11 11:00:52 +0100

    Fix static subtract method and add test for it

--- source/subtract.js
+++ source/subtract.js
@@ -1,10 +1,15 @@
 import momentFromString from './index'
 import subtractDurationFromInstant from './subtractDurationFromInstant'
+import trimToPrecision from './trimToPrecision'
 
 
 // TODO: Convert to static Moment class when native es2015 support arrives
 export default (moment, duration) => {
 	return momentFromString(
-		subtractDurationFromInstant(moment.lowerLimit, duration).toISOString()
+		trimToPrecision(
+			subtractDurationFromInstant(moment.lowerLimit, duration)
+				.toISOString(),
+			duration.precision
+		)
 	)
 }
--- test/addAndSubtract.js
+++ test/addAndSubtract.js
@@ -2,8 +2,8 @@ import runTest from 'ava'
 import expect from 'unexpected'
 import Duration from '@datatypes/duration'
 
+import momentFromString, {add, subtract} from '../source/index'
 import Instant from '../source/classes/Instant'
-import momentFromString, {add} from '../source/index'
 
 
 runTest('add duration to moment', test => {
@@ -13,7 +13,21 @@ runTest('add duration to moment', test => {
 	const newMomentObject = {
 		string: '2015-11-25T01Z',
 		lowerLimit: new Instant('2015-11-25T01'),
-		upperLimit: new Instant('2015-11-25T02')
+		upperLimit: new Instant('2015-11-25T02'),
+	}
+
+	expect(newMoment.object, 'to equal', newMomentObject)
+})
+
+
+runTest('subtract duration from moment', test => {
+	const moment = momentFromString('2015-11-25')
+	const oneHour = new Duration('P1H')
+	const newMoment = subtract(moment, oneHour)
+	const newMomentObject = {
+		string: '2015-11-24T23Z',
+		lowerLimit: new Instant('2015-11-24T23'),
+		upperLimit: new Instant('2015-11-25T00'),
 	}
 
 	expect(newMoment.object, 'to equal', newMomentObject)
